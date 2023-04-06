085caa23e6b3c9375ed5a28ba51da22fc0ca418a
Antonio <ap66484@gmail.com>
2023-04-06 14:47:28 -0300

    criando componente de acessibilidade

---
+++ projetoestagio/components/Acessibilidade/Acessibilidade.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from './Acessibilidade.module.css'
+
+const acessibilidade =()=>{
+    return(
+        <div className={styles.caixa}>
+
+
+        </div>
+    )
+}
