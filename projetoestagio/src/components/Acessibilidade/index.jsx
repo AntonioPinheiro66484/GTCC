dc7317b95fa86a99586f9226ae58fde42ee152eb
Antonio Gabriel <ap66484@gmail.com>
2023-04-17 13:12:49 -0300

    criando e inserindo o componente de acessibilidade

---
+++ projetoestagio/src/components/Acessibilidade/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from './Acessibilidade.module.css'
+
+const Acessibilidade =()=>{
+    return(
+        <div className={styles.barraAcessibilidade}>
+            <div className={styles.caixaFerramentas}>
+                <div className={styles.itemFerramenta}>Acessibilidade</div>
+                <div className={styles.itemFerramenta}>Alto Contraste</div>
+                <div className={styles.itemFerramenta}>A+</div>
+                <div className={styles.itemFerramenta}>A-</div>
+            </div>   
+        </div>
+    )
+}
+
+export default Acessibilidade
