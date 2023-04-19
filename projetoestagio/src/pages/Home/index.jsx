a6aa6ed0d8af5bde4538b06f1bc54629b9c01bd6
Antonio Gabriel <Antonio Gabriel>
2023-04-19 15:06:32 -0300

    adição de alguns componentes na pagina home

--- projetoestagio/src/pages/Home/index.jsx
+++ projetoestagio/src/pages/Home/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-import style from './index'
+import styles from './index.module.css/'
 import Acessibilidade from '../../components/Acessibilidade'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
 
 const Home =()=>{
-    <Acessibilidade></Acessibilidade>
+    <div className={styles.caixa}>
+        <Acessibilidade></Acessibilidade>
+        <Header></Header>
+        <NavBar></NavBar>
+        <SearchBar></SearchBar>
+        <Footer></Footer>
+    </div>
 }
 
+export default Home
