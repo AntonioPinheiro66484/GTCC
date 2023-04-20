151b3c3cc4ac31b808ced08e0e31cd112b2ac0a9
Antonio Gabriel Pinheiro Lima <ap66484@gmail.com>
2023-04-20 15:35:40 -0300

    arrumando reenderização do footer e ajustando as imagens

--- projetoestagio/src/components/Header/index.jsx
+++ projetoestagio/src/components/Header/index.jsx
@@ -1,14 +1,14 @@
 import React from "react"
 import styles from './Header.module.css'
-import LogoFatec from '../../../assets/images/LogoFatecZL.png'
-import LogoGoverno from '../../../assets/images/LogoGovernoeCps.png'
+import LogoFatec from '../../assets/images/LogoFatecZL.png'
+import LogoGoverno from '../../assets/images/LogoGovernoeCps.png'
 
 const Header =()=>{
-    return(
-        <div className={styles.caixa}>
-            <img className={styles.logo} src={LogoFatec}></img>
-            <img className={styles.logo} src={LogoGoverno}></img>
-        </div>
+     return(
+         <div className={styles.caixa}>
+            <img className={styles.logoFatec} src={LogoFatec}/>
+            <img className={styles.logoGoverno} src={LogoGoverno}/>
+         </div>
     )
 }
 
