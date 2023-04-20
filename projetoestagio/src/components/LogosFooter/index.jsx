516effe82156aad6e784f8249d3d655f6e11ae11
Antonio Gabriel Pinheiro Lima <ap66484@gmail.com>
2023-04-20 15:47:24 -0300

    mudança de nome do logo para logo footer

---
+++ projetoestagio/src/components/LogosFooter/index.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import styles from './LogosFooter.module.css'
+import LogoFatec from '../../assets/images/LogoFatecZL.png'
+import LogoGoverno from '../../assets/images/LogoGovernoeCps.png'
+
+const Footer =()=>{
+    return(
+        <div className={styles.caixa}>
+            <img className={styles.logoFatec} src={LogoFatec}/>
+            <img className={styles.logoGoverno} src={LogoGoverno}/>
+        </div>
+    )
+}
+
+export default Footer
