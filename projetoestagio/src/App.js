1af2321ace97ed53203394a238930194a227cdac
Antonio Gabriel Pinheiro Lima <ap66484@gmail.com>
2023-04-20 15:32:54 -0300

    adicionamos o return no app.js

--- projetoestagio/src/App.js
+++ projetoestagio/src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
-import Home from './pages/Home'
+import Home from './pages/Home';
 import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
      <Router>
       <Routes>
-        <Route path="/" element={<Home/>}>
+        <Route path="/" element={<Home/>}/>
 
-        </Route>
       </Routes>
      </Router>
    
