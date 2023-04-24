83007c6b1401801359c1ce61b7c2531f0ec9beb2
Antonio <ap66484@gmail.com>
2023-04-24 15:45:33 -0300

    Criando componente e adicionando estilo do MapaFooter para o site

--- projetoestagio/src/components/MapaFooter/index.jsx
+++ projetoestagio/src/components/MapaFooter/index.jsx
@@ -3,15 +3,24 @@ import styles from './MapaFooter.module.css'
 
 const MapaFooter =()=>{
     return(
-        <div className={styles.caixa}>
-            <ul>
-                <li>Trabalho</li>
-                <li><a>Meu trabalho</a></li>
-                <li><a>Integrantes</a></li>
-                <li><a>Orientador</a></li>
-                <li><a>Etapas</a></li>
-            </ul>
-        </div>
+            <div className={styles.caixa}>
+                <div className={styles.caixaLista}>
+                    <ul className={styles.lista}>
+                        <li><h2 className={styles.titulo}>Trabalho</h2></li>
+                        <li><a>Meu trabalho</a></li>
+                        <li><a>Integrantes</a></li>
+                        <li><a>Orientador</a></li>
+                        <li><a>Etapas</a></li>
+                    </ul>
+                    <ul className={styles.lista}>
+                        <li><h2 className={styles.titulo}>Contatos</h2></li>
+                        <li><a></a></li>
+                        <li><a></a></li>
+                        <li><a></a></li>
+                        <li><a></a></li>
+                    </ul>
+                </div>
+            </div>
     )
 }
 
