db816b82bc1e1bf2487d1b9b76ca326c9d5cbadb
Antonio Gabriel Pinheiro Lima <ap66484@gmail.com>
2023-04-25 15:40:15 -0300

    componente de barra de pesquisa ajuste no estilo e adição de imagem de lupa

--- projetoestagio/src/components/SearchBar/index.jsx
+++ projetoestagio/src/components/SearchBar/index.jsx
@@ -1,10 +1,19 @@
 import React from "react"
 import styles from "./SearchBar.module.css"
+import { Icon } from '@iconify/react';
 
 const SearchBar =()=>{
     return(
+        // <div className={styles.caixa}>
+        //     <input className={styles.searchBar} placeholder="Pesquisar"/>
+        // </div>
         <div className={styles.caixa}>
-            <input className={styles.searchBar} placeholder="Pesquisar"/>
+            <fieldset className={styles.searchBar}>
+                <input className={styles.searchInput} placeholder="Pesquisar"/>
+                <button className={styles.searchButton}>
+                    <Icon icon="ph:magnifying-glass-bold"/>
+                </button>
+            </fieldset>
         </div>
     ) 
 }
