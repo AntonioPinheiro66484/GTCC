7056da4f67d124108ca397974d356ac262ed6a7e
Antonio Gabriel Pinheiro Lima <ap66484@gmail.com>
2023-04-26 13:45:52 -0300

    blocos de progresso começando a ser implementados

--- projetoestagio/src/components/Conteudos/ConteudoAluno/index.jsx
+++ projetoestagio/src/components/Conteudos/ConteudoAluno/index.jsx
@@ -22,18 +22,46 @@ const ConteudoAluno=()=>{
                 <div className={styles.caixaDescricao}>
                     <div className={styles.caixaBloco}>
                         <div className={styles.bloco}>
-                            <h2 className={styles.tituloBloco}>Descrição</h2>
-                            <p className={styles.textoBloco}>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <h2 className={styles.tituloBloco}>Etapa 1</h2>
+                            <p className={styles.textoBlocoProgresso}>Definição de identidade do projeto</p>
+                            <p className={styles.textoBlocoProgresso}>Entrega até segunda-feira, 27 de março</p>
+                            <p className={styles.textoBlocoProgresso}>Conclusão</p>
                         </div>
                     </div>
-                </div>
+                {/* </div>
                 <div className={styles.caixaProgresso}>
                     <div className={styles.caixaBloco}>
                         <div className={styles.bloco}>
-                            <h2 className={styles.tituloBloco}>Descrição</h2>
-                            <p className={styles.textoBloco}>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <h2 className={styles.tituloBloco}>Etapa 2</h2>
+                            <p className={styles.textoBlocoProgresso}>Definição de identidade do projeto</p>
+                            <p className={styles.textoBlocoProgresso}>Entrega até segunda-feira, 27 de março</p>
+                            <p className={styles.textoBlocoProgresso}>Conclusão</p>
                         </div>
                     </div>
+                    <div className={styles.caixaBloco}>
+                        <div className={styles.bloco}>
+                            <h2 className={styles.tituloBloco}>Etapa 3</h2>
+                            <p className={styles.textoBlocoProgresso}>Definição de identidade do projeto</p>
+                            <p className={styles.textoBlocoProgresso}>Entrega até segunda-feira, 27 de março</p>
+                            <p className={styles.textoBlocoProgresso}>Conclusão</p>
+                        </div>
+                    </div>
+                    <div className={styles.caixaBloco}>
+                        <div className={styles.bloco}>
+                            <h2 className={styles.tituloBloco}>Etapa 4</h2>
+                            <p className={styles.textoBlocoProgresso}>Definição de identidade do projeto</p>
+                            <p className={styles.textoBlocoProgresso}>Entrega até segunda-feira, 27 de março</p>
+                            <p className={styles.textoBlocoProgresso}>Conclusão</p>
+                        </div>
+                    </div>
+                    <div className={styles.caixaBloco}>
+                        <div className={styles.bloco}>
+                            <h2 className={styles.tituloBloco}>Etapa 5</h2>
+                            <p className={styles.textoBlocoProgresso}>Definição de identidade do projeto</p>
+                            <p className={styles.textoBlocoProgresso}>Entrega até segunda-feira, 27 de março</p>
+                            <p className={styles.textoBlocoProgresso}>Conclusão</p>
+                        </div>
+                    </div> */}
                 </div>
                 
         </div>
