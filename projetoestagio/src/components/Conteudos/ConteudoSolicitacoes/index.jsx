6e2c2633bbef1801768f9889a342bd090fb5b6ef
Antonio Gabriel Pinherio Lima <ap66484@gmail.com>
2023-04-28 15:18:10 -0300

    criando componente de conteudo da pagina solicitaçoes e estilizando

---
+++ projetoestagio/src/components/Conteudos/ConteudoSolicitacoes/index.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from './ConteudoSolicitacoes.module.css'
+
+const ConteudoSolicitacoes =()=>{
+    return(
+        <div className={styles.caixa}>
+            <h1 className={styles.tituloPrincipal}>Solicitações</h1>
+            <div className={styles.caixaBloco}>
+                <div className={styles.bloco}>
+                    <div className={styles.caixaTexto}>
+                        <h2 className={styles.tituloBloco}>Site 1</h2>
+                        <p className={styles.textoBloco}>Projeto web • Acessibilidade em sites web</p>
+                        <p className={styles.textoBloco}>Criado por Ana, Carlos, João</p>
+                    </div>
+                    <div className={styles.caixaBotao}>
+                        <button className={styles.botaoConcluir}>
+                            Aceitar
+                        </button>
+                        <button className={styles.botaoConcluir}>
+                            Recusar
+                        </button>
+                    </div>
+                </div>
+                <div className={styles.bloco}>
+                    <div className={styles.caixaTexto}>
+                        <h2 className={styles.tituloBloco}>Site 2</h2>
+                        <p className={styles.textoBloco}>PProjeto web • E-commerce de livros</p>
+                        <p className={styles.textoBloco}>Criado por Ana, Carlos, João</p>
+                    </div>
+                    <div className={styles.caixaBotao}>
+                        <button className={styles.botaoConcluir}>
+                            Aceitar
+                        </button>
+                        <button className={styles.botaoConcluir}>
+                            Recusar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ConteudoSolicitacoes
