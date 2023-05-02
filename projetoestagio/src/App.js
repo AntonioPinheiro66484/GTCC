b1d96ca967b6b930c052137fade7b435d077a365
antonio gabriel <app66484@gmail.com>
2023-05-02 15:41:48 -0300

    colocando colocando as rotas para pagina de trabalhos e pagina de editar

--- projetoestagio/src/App.js
+++ projetoestagio/src/App.js
@@ -6,23 +6,26 @@ import PaginaIntegrantes from './pages/PaginaIntegrantes'
 import PaginaEtapas from './pages/PaginaEtapas'
 import PaginaOrientador from './pages/PaginaOrientador'
 import PaginaSolicitacoes from './pages/PaginaSolicitacoes'
+import PaginaTrabalhos from './pages/PaginaTrabalhos'
+import PaginaEditar from './pages/PaginaEditar'
 
 function App() {
   return (
     <div className="App">
-     <Router>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/PaginaTrabalho" element={<PaginaTrabalho/>}/>
-        <Route path="/PaginaIntegrantes" element={<PaginaIntegrantes/>}/>
-        <Route path='/PaginaEtapas' element={<PaginaEtapas/>}/>
-        <Route path='/PaginaOrientador' element={<PaginaOrientador/>}/>
-        <Route path='/PaginaSolicitacoes' element={<PaginaSolicitacoes/>}/>
-      </Routes>
-     </Router>
-   
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/PaginaTrabalho" element={<PaginaTrabalho/>}/>
+          <Route path="/PaginaIntegrantes" element={<PaginaIntegrantes/>}/>
+          <Route path='/PaginaEtapas' element={<PaginaEtapas/>}/>
+          <Route path='/PaginaOrientador' element={<PaginaOrientador/>}/>
+          <Route path='/PaginaSolicitacoes' element={<PaginaSolicitacoes/>}/>
+          <Route path='/PaginaTrabalhos' element={<PaginaTrabalhos/>}/>
+          <Route path='/PaginaEditar' element={<PaginaEditar/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
 
-export default App;
+export default App
