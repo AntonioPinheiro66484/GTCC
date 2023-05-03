12c6ac674dba051e9759ff287f696ddbf3655b20
Antonio <ap66484@gmail.com>
2023-05-03 15:51:52 -0300

    colocando a barra de pesquisas

--- projetoestagio/src/pages/PaginaOrientadores/index.jsx
+++ projetoestagio/src/pages/PaginaOrientadores/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Acessibilidade from '../../components/Acessibilidade'
 import Header from '../../components/Header'
 import NavBar from '../../components/NavBar'
+import SearchBar from '../../components/SearchBar'
 import LogosFooter from '../../components/LogosFooter'
 import MapaFooter from '../../components/MapaFooter'
 import InfoFooter from '../../components/InfoFooter'
@@ -13,6 +14,7 @@ const PaginaOrientadores =()=>{
             <Acessibilidade/>
             <Header/>
             <NavBar/>
+            <SearchBar/>
             <ConteudoOrientadores/>
             <MapaFooter/>
             <LogosFooter/>
