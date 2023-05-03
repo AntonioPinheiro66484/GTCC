50876e19f0a07c189534a49b76469ae9a1660250
Antonio <ap66484@gmail.com>
2023-05-03 15:48:57 -0300

    renomeando as classes das labels

--- projetoestagio/src/components/Conteudos/ConteudoLogin/index.jsx
+++ projetoestagio/src/components/Conteudos/ConteudoLogin/index.jsx
@@ -1,20 +1,23 @@
-import React from "react";
+import React from "react"
 import styles from './ConteudoLogin.module.css'
 
-const ConteudoLogin=()=>{
+const ConteudoLogin =()=>{
     return(
         <div className={styles.caixa}>
-             <h1 className={styles.tituloPrincipal}>Cadastre seu TCC</h1>
+            <div className={styles.bloco}>
                 <form className={styles.formulario}>
+                    <h1 className={styles.tituloPrincipal}>Entre</h1>
                     <div className={styles.caixaInput}>
-                        <label><h2 className={styles.tituloSecundario}>RA</h2></label>
-                        <input  className={styles.input} type="text"/>
+                        <label className={styles.labelPrincipal}>RA</label>
+                        <input className={styles.input} type="text"/>
                     </div>
                     <div className={styles.caixaInput}>
-                        <label><h2 className={styles.tituloSecundario}>Senha</h2></label>
-                        <input  className={styles.input} type="text"/>
+                        <label className={styles.labelPrincipal}>Senha</label>
+                        <input className={styles.input} type="password"/>
                     </div>
+                    <button className={styles.botao}>Entrar</button>
                 </form>
+            </div>
         </div>
     )
 }
