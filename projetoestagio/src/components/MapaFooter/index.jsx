2677ecb172c67c95b81b4511cb8e1c1e533edcc1
Antonio <ap66484@gmail.com>
2023-05-05 14:17:48 -0300

    ajuste de links no mapafooter

--- projetoestagio/src/components/MapaFooter/index.jsx
+++ projetoestagio/src/components/MapaFooter/index.jsx
@@ -7,15 +7,15 @@ const MapaFooter =()=>{
                 <div className={styles.caixaLista}>
                     <ul className={styles.lista}>
                         <li><h2 className={styles.tituloLista}>Trabalho</h2></li>
-                        <li><a className={styles.itemLista}>Meu trabalho</a></li>
-                        <li><a className={styles.itemLista}>Integrantes</a></li>
-                        <li><a className={styles.itemLista}>Orientador</a></li>
-                        <li><a className={styles.itemLista}>Etapas</a></li>
+                        <li><a href="/PaginaTrabalho" className={styles.itemLista}>Meu trabalho</a></li>
+                        <li><a href="/PaginaIntegrantes" className={styles.itemLista}>Integrantes</a></li>
+                        <li><a href="PaginaOrientador"className={styles.itemLista}>Orientador</a></li>
+                        <li><a href="/PaginaEtapas" className={styles.itemLista}>Etapas</a></li>
+                        <li><a href="/PaginaTrabalhos" className={styles.itemLista}>Trabalhos</a></li>
                     </ul>
                     <ul className={styles.lista}>
                         <li><h2 className={styles.tituloLista}>Suporte</h2></li>
-                        <li><a className={styles.itemLista}>Tem alguma dúvida?</a></li>
-                        <li><a className={styles.itemLista}>Fale conosco</a></li>
+                        <li><a href="http://suporte.fateczl.edu.br/" className={styles.itemLista}>Abra um chamado</a></li>
                     </ul>
                 </div>
             </div>
