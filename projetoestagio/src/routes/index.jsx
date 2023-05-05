0e3a97f61e68f65fa3708cd257fec990185bbd77
Antonio <ap66484@gmail.com>
2023-05-05 15:22:47 -0300

    criação da pagina de rotas

---
+++ projetoestagio/src/routes/index.jsx
@@ -0,0 +1,32 @@
+import Home from '../pages/Home'
+import PaginaTrabalho from '../pages/PaginaTrabalho'
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
+import PaginaIntegrantes from '../pages/PaginaIntegrantes'
+import PaginaEtapas from '../pages/PaginaEtapas'
+import PaginaOrientador from '../pages/PaginaOrientador'
+import PaginaSolicitacoes from '../pages/PaginaSolicitacoes'
+import PaginaTrabalhos from '../pages/PaginaTrabalhos'
+import PaginaEditar from '../pages/PaginaEditar'
+import PaginaLogin from '../pages/PaginaLogin'
+import PaginaOrientadores from '../pages/PaginaOrientadores'
+
+function Rotas() {
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/PaginaTrabalho" element={<PaginaTrabalho/>}/>
+          <Route path="/PaginaIntegrantes" element={<PaginaIntegrantes/>}/>
+          <Route path='/PaginaEtapas' element={<PaginaEtapas/>}/>
+          <Route path='/PaginaOrientador' element={<PaginaOrientador/>}/>
+          <Route path='/PaginaSolicitacoes' element={<PaginaSolicitacoes/>}/>
+          <Route path='/PaginaTrabalhos' element={<PaginaTrabalhos/>}/>
+          <Route path='/PaginaEditar' element={<PaginaEditar/>}/>
+          <Route path='/PaginaLogin' element={<PaginaLogin/>}/>
+          <Route path='/PaginaOrientadores' element={<PaginaOrientadores/>}/>
+        </Routes>
+      </Router>
+  );
+}
+
+export default Rotas
