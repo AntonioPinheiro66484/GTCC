92bed39ae7367eef7bfa7b850e18579a83dbb9fb
Antonio Gabriel Pinheiro Lima <79329807+AntonioPinheiro66484@users.noreply.github.com>
2023-05-08 13:26:49 -0300

    A

---
+++ projetoestagio/src/pages/PaginaOrientador/index.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import styles from './styles.module.css'
+
+const PaginaOrientador =()=>{
+    return(
+        <div className={styles.caixa}>
+            <h1 className={styles.tituloPrincipal}>Orientador</h1>
+            <div className={styles.caixaBloco}>
+                <div className={styles.bloco}>
+                    <h2 className={styles.tituloBloco}>Maria Fulana Lima</h2>
+                    <p className={styles.textoBloco}>maria.lima43@fatec.sp.gov.br</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PaginaOrientador
---
+++ projetoestagio/src/pages/PaginaSolicitacoes/index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from './styles.module.css'
+
+const PaginaSolicitacoes =()=>{
+    return(
+        <div className={styles.caixa}>
+            <h1 className={styles.tituloPrincipal}>Solicitações</h1>
+            <div className={styles.caixaBloco}>
+                <div className={styles.bloco}>
+                    <div className={styles.caixaTexto}>
+                        <h2 className={styles.tituloBloco}>Site 1</h2>
+                        <p className={styles.textoBloco}>Projeto web • Acessibilidade em sites web</p>
+                        <p className={styles.textoBloco}>Criado por Ana, Carlos, João</p>
+                    </div>
+                    <div className={styles.caixaBotao}>
+                        <button className={styles.botao}>Aceitar</button>
+                        <button className={styles.botao}>Recusar</button>
+                    </div>
+                </div>
+                <div className={styles.bloco}>
+                    <div className={styles.caixaTexto}>
+                        <h2 className={styles.tituloBloco}>Site 2</h2>
+                        <p className={styles.textoBloco}>PProjeto web • E-commerce de livros</p>
+                        <p className={styles.textoBloco}>Criado por Ana, Carlos, João</p>
+                    </div>
+                    <div className={styles.caixaBotao}>
+                        <button className={styles.botao}>Aceitar</button>
+                        <button className={styles.botao}>Recusar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PaginaSolicitacoes
