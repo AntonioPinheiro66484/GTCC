58c7febc750e10215c8404877aba55062b48529d
Antonio Gabriel Pinheiro Lima <Antonio Gabriel Pinheiro Lima>
2023-05-09 15:21:52 -0300

    criando componente e estilo da pagina de cadastrar Tipo de trabalho

---
+++ projetoestagio/src/pages/CadastrarTipoTrabalho/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+const CadastrarTipoTrabalho =()=>{
+    return(
+        <div className={styles.caixa}>
+            <h1 className={styles.tituloPrincipal}>Cadastrar tipo de trabalho</h1>
+            <form className={styles.formulario}>
+                <div className={styles.caixaInput}>
+                    <label className={styles.labelPrincipal}>Nome</label>
+                    <input className={styles.input} type="text"/>
+                </div>
+                <div className={styles.caixaInput}>
+                    <label className={styles.labelPrincipal}>Máximo de integrantes por grupo</label>
+                    <input className={styles.input} type="text"/>
+                </div>
+                <div className={styles.caixaInput}>
+                    <label className={styles.labelPrincipal}>Cursos</label>
+                    <input className={styles.input} type="text"/>
+                </div>
+                <div className={styles.caixaBotao}>
+                    <button className={styles.botao}>Salvar</button>
+                    <button className={styles.botao}>Cancelar</button>
+                </div>
+            </form>      
+        </div>
+    )
+}
+
+export default CadastrarTipoTrabalho
