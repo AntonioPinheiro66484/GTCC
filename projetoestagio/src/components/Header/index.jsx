6af438182b80c25992b95926c318eb612f94236c
Antonio Gabriel Pinheiro Lima <Antonio Gabriel Pinheiro Lima>
2023-05-09 15:19:50 -0300

    importando o botão no header e estilizando o menu/header

--- projetoestagio/src/components/Header/index.jsx
+++ projetoestagio/src/components/Header/index.jsx
@@ -2,12 +2,20 @@ import React from "react"
 import styles from './styles.module.css'
 import LogoFatec from '../../assets/images/LogoFatecZL.png'
 import LogoGoverno from '../../assets/images/LogoGovernoeCps.png'
+import {Icon} from '@iconify/react';
 
 const Header =()=>{
      return(
          <div className={styles.caixa}>
-            <img className={styles.logoFatec} src={LogoFatec}/>
-            <img className={styles.logoGoverno} src={LogoGoverno}/>
+            <div>
+                <button className={styles.menuHamburguer}>
+                    <Icon icon="ic:baseline-menu"/>
+                </button>
+            </div>
+            <div>
+                <img className={styles.logoFatec} src={LogoFatec}/>
+                <img className={styles.logoGoverno} src={LogoGoverno}/>
+            </div>
          </div>
     )
 }
