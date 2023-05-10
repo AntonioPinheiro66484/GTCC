ae080ed2675e5d7d685d42a9e5ff3debf852f0e7
algo <ap66484@gmail.com>
2023-05-10 14:59:38 -0300

    componente botão e estilo

---
+++ projetoestagio/src/components/Botao/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+const Botao =()=>{
+    return (
+        <button className={styles.botao}>{prop.label}</button>
+    )
+}
+
+export default Botao
