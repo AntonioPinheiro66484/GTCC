8be00279235e137775db55cdf714ae017b05b5ea
felipe-coletti <felipecoletti2@gmail.com>
2023-05-16 14:11:31 -0300

    Aumentar ou diminuir o tamanho da fonte.

--- projetoestagio/src/components/BarraAcessibilidade/index.jsx
+++ projetoestagio/src/components/BarraAcessibilidade/index.jsx
@@ -2,13 +2,29 @@ import React from 'react'
 import styles from './styles.module.css'
 
 const BarraAcessibilidade =()=>{
+    var tamanhoFonte = 16
+
+    const aumentarFonte =()=>{
+        if (tamanhoFonte < 20) {
+            tamanhoFonte++
+            document.body.style.fontSize = tamanhoFonte + "px"
+        }
+    }
+
+    const diminuirFonte =()=>{
+        if (tamanhoFonte > 14) {
+            tamanhoFonte--
+            document.body.style.fontSize = tamanhoFonte + "px"
+        }
+    }
+
     return(
         <div className={styles.barraAcessibilidade}>
             <ul className={styles.caixaItens}>
                 <li className={styles.item}><a className={styles.linkItem} href="/acessibilidade">Acessibilidade</a></li>
                 <li className={styles.item}>Alto Contraste</li>
-                <li className={styles.item}>A+</li>
-                <li className={styles.item}>A-</li>
+                <li className={styles.item} onClick={aumentarFonte}>A+</li>
+                <li className={styles.item} onClick={diminuirFonte}>A-</li>
             </ul>
         </div>
     )
