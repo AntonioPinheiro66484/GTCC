fbf3ccc847787ce7a5b30687649773c1c5d1c7e5
Antonio Gabriel Pinheiro Lima <ap66484@gmail.com>
2023-05-18 15:35:56 -0300

    inicio de funções

--- projetoestagio/src/components/BarraAcessibilidade/index.jsx
+++ projetoestagio/src/components/BarraAcessibilidade/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./styles.module.css";
-import { aumentarFonte,diminuirFonte } from "../static/fonte";
+import { aumentarFonte,diminuirFonte } from "../../static/fonte";
+import { mudarContraste } from "../../static/modoContraste";
 
 const BarraAcessibilidade =()=>{
     return(
         <div className={styles.barraAcessibilidade}>
             <ul className={styles.caixaItens}>
                 <li className={styles.item}><a className={styles.linkItem} href="/acessibilidade">Acessibilidade</a></li>
-                <li className={styles.item}>Alto Contraste</li>
+                <li className={styles.item} onClick={mudarContraste}>Alto Contraste</li>
                 <li className={styles.item} onClick={aumentarFonte}>A+</li>
                 <li className={styles.item} onClick={diminuirFonte}>A-</li>
             </ul>
---
+++ projetoestagio/src/static/modoContraste.js
@@ -0,0 +1,9 @@
+// var contraste = false
+
+const mudarContraste =()=> {
+    let list = document.querySelector('root');
+    
+    list.classList.toggle('dark')
+}
+
+export{mudarContraste}
