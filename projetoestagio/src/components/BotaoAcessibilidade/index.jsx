d34a7924d1202a106725d1e7fbc812e4646674e5
Antonio Gabriel Pinheiro Lima <ap66484@gmail.com>
2023-05-24 14:06:57 -0300

    uso de useState no botao de acessibilidade mobile

--- projetoestagio/src/components/BotaoAcessibilidade/index.jsx
+++ projetoestagio/src/components/BotaoAcessibilidade/index.jsx
@@ -1,33 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./styles.module.css";
 import {Icon} from "@iconify/react";
 import { aumentarFonte,diminuirFonte } from "../../static/fonte";
+
 const BotaoAcessibilidade =()=>{
+    const [botaoAcessibilidade, setBotaoAcessibilidade] = useState(false)
+
+    const showBotaoAcessibilidade = () => setBotaoAcessibilidade(!botaoAcessibilidade)
+
     return (
-        <ul className={styles.caixa}>
-            <li>
-                <div className={styles.botaoPrincipal}>
-                    <Icon icon="lucide:person-standing"/>
-                </div>
-                <ul className={styles.caixaOpcoes}>
-                    <li >
-                        <button className={styles.botaoSecundario}>
-                            <Icon icon="material-symbols:contrast"/>
-                        </button>
-                    </li>
-                    <li>
-                        <button onClick={aumentarFonte} className={styles.botaoSecundario}>
-                            <Icon icon="fluent-mdl2:font-increase"/>
-                        </button>
-                    </li>
-                    <li>
-                        <button onClick={diminuirFonte} className={styles.botaoSecundario}>
-                            <Icon icon="fluent-mdl2:font-decrease"/>
-                        </button>
-                    </li>
-                </ul>
-            </li>
-        </ul>
+        <div className={styles.caixa}>
+            <ul className={botaoAcessibilidade ? styles.caixaOpcoesAtivado : styles.caixaOpcoesDesativado}>
+                <li>
+                    <button className={styles.botaoSecundario}>
+                        <Icon icon="material-symbols:contrast"/>
+                    </button>
+                </li>
+                <li>
+                    <button onClick={aumentarFonte} className={styles.botaoSecundario}>
+                        <Icon icon="fluent-mdl2:font-increase"/>
+                    </button>
+                </li>
+                <li>
+                    <button onClick={diminuirFonte} className={styles.botaoSecundario}>
+                        <Icon icon="fluent-mdl2:font-decrease"/>
+                    </button>
+                </li>
+            </ul>
+            <div className={styles.botaoPrincipal} onClick={showBotaoAcessibilidade}>
+                <Icon icon="lucide:person-standing"/>
+            </div>
+        </div>
     )
 }
 
