084c106491fe215ca5215699b301046805a1af51
antonio <ap66484@gmail.com>
2023-06-06 15:50:20 -0300

    remoção de icone não utilizado

--- projetoestagio/src/pages/TiposTrabalho/index.jsx
+++ projetoestagio/src/pages/TiposTrabalho/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./styles.module.css";
-import { Icon } from "@iconify/react";
 import MenuOpcoes from "../../components/MenuOpcoes";
 
 const TiposTrabalho =()=>{
