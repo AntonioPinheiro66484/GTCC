29a870b689930e3695aac91bd1ab12aae1f6d315
Antonio <ao66484@gmail.com>
2023-06-07 13:12:13 -0300

    ajuste no padrao de formatação

--- projetoestagio/src/components/MapaFooter/index.jsx
+++ projetoestagio/src/components/MapaFooter/index.jsx
@@ -7,17 +7,17 @@ const MapaFooter =()=>{
             <div className={styles.caixaLista}>
                 <h2 className={styles.tituloLista}>Trabalho</h2>
                 <ul className={styles.lista}>
-                    <li><a href="/trabalho" className={styles.itemLista}>Meu trabalho</a></li>
-                    <li><a href="/integrantes" className={styles.itemLista}>Integrantes</a></li>
-                    <li><a href="/orientador"className={styles.itemLista}>Orientador</a></li>
-                    <li><a href="/etapas" className={styles.itemLista}>Etapas</a></li>
-                    <li><a href="/trabalhos" className={styles.itemLista}>Trabalhos</a></li>
+                    <li><a className={styles.itemLista} href="/trabalho">Meu trabalho</a></li>
+                    <li><a className={styles.itemLista} href="/integrantes">Integrantes</a></li>
+                    <li><a className={styles.itemLista} href="/orientador">Orientador</a></li>
+                    <li><a className={styles.itemLista} href="/etapas">Etapas</a></li>
+                    <li><a className={styles.itemLista} href="/trabalhos">Trabalhos</a></li>
                 </ul>
             </div>
             <div className={styles.caixaLista}>
                 <h2 className={styles.tituloLista}>Suporte</h2>
                 <ul className={styles.lista}>
-                    <li><a href="http://suporte.fateczl.edu.br/" className={styles.itemLista}>Abra um chamado</a></li>
+                    <li><a className={styles.itemLista} href="http://suporte.fateczl.edu.br/" target="_blank" rel="noopener noreferrer">Abra um chamado</a></li>
                 </ul>
             </div>    
         </div>
