2f35d28a9280d581bebbfa7cae35be16128d5b77
antonio <ap66484@gmail.com>
2023-06-12 14:18:59 -0300

    troca de ordem dos itens da lista

--- projetoestagio/src/components/MapaFooter/index.jsx
+++ projetoestagio/src/components/MapaFooter/index.jsx
@@ -8,9 +8,9 @@ const MapaFooter =()=>{
                 <h2 className={styles.tituloLista}>Trabalho</h2>
                 <ul className={styles.lista}>
                     <li><a className={styles.itemLista} href="/trabalho">Meu trabalho</a></li>
+                    <li><a className={styles.itemLista} href="/etapas">Etapas</a></li>
                     <li><a className={styles.itemLista} href="/integrantes">Integrantes</a></li>
                     <li><a className={styles.itemLista} href="/orientador">Orientador</a></li>
-                    <li><a className={styles.itemLista} href="/etapas">Etapas</a></li>
                     <li><a className={styles.itemLista} href="/trabalhos">Trabalhos</a></li>
                 </ul>
             </div>
