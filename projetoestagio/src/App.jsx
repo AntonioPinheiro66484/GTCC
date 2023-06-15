6492c91b1c2a1416b91226aeb070bb09efba6efa
antonio <ap66484@gmail.com>
2023-06-15 15:48:46 -0300

    comentamos os componentes não utilizados

--- projetoestagio/src/App.jsx
+++ projetoestagio/src/App.jsx
@@ -3,9 +3,9 @@ import BarraAcessibilidade from "./components/BarraAcessibilidade";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import BotaoAcessibilidade from "./components/BotaoAcessibilidade";
-import MapaFooter from "./components/MapaFooter";
+/*import MapaFooter from "./components/MapaFooter";
 import LogosFooter from "./components/LogosFooter";
-import InfoFooter from "./components/InfoFooter";
+import InfoFooter from "./components/InfoFooter";*/
 
 function App() {
     return (
