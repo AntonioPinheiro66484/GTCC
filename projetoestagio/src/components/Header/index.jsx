f8f6b839a9050f07a8534c886065e0eae955f169
antonio <ap66484@gmail.com>
2023-06-15 15:19:16 -0300

    retirada do componente barra de pesquisa

--- projetoestagio/src/components/Header/index.jsx
+++ projetoestagio/src/components/Header/index.jsx
@@ -4,6 +4,7 @@ import LogoFatec from "../../assets/images/LogoFatecZL.png";
 import LogoGoverno from "../../assets/images/LogoGovernoeCps.png";
 import { Icon } from "@iconify/react";
 import NavBarMobile from "../NavBarMobile";
+import BarraPesquisa from "../BarraPesquisa";
 
 const Header =()=>{
     const [navBar, setNavBar] = useState(false)
@@ -14,9 +15,12 @@ const Header =()=>{
         <div className={styles.caixa}>
             <Icon className={styles.menuHamburguer} icon="ic:baseline-menu" onClick={showNavBar}/>
             {navBar && <NavBarMobile active={setNavBar}/>}
-            <div className={styles.caixaLogo}>
+            <BarraPesquisa/>
+            {/*<div className={styles.caixaLogo}>
                 <img className={styles.logoFatec} alt="Fatec Zona Leste" src={LogoFatec}/>
-                <img className={styles.logoGoverno} alt="Centro Paula Souza, Governo do Estado de São Paulo" src={LogoGoverno}/>
+            </div>*/}
+            <div className={styles.caixaFerramentas}>
+                <img className={styles.perfil} alt="Perfil" src={LogoFatec}/>
             </div>
         </div>
     )
