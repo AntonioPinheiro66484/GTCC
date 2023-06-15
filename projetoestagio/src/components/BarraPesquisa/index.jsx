7c30dd88da3a712b00b603f40e7dd927c26f45eb
antonio <ap66484@gmail.com>
2023-06-15 15:49:11 -0300

    retirando comentarios

--- projetoestagio/src/components/BarraPesquisa/index.jsx
+++ projetoestagio/src/components/BarraPesquisa/index.jsx
@@ -4,14 +4,14 @@ import { Icon } from "@iconify/react";
 
 const BarraPesquisa =()=>{
     return(
-        <div className={styles.caixa}>
-            <fieldset className={styles.searchBar}>
+        /*<div className={styles.caixa}>*/
+            <div className={styles.searchBar}>
                 <input className={styles.searchInput} placeholder="Pesquisar"/>
                 <button className={styles.searchButton}>
                     <Icon icon="ph:magnifying-glass-bold"/>
                 </button>
-            </fieldset>
-        </div>
+            </div>
+        /*</div>*/
     ) 
 }
 
