e2bd576e8ad2c4957f2edcb2c93cc74f954c4391
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-06-24 16:17:15 -0300

    Ajuste no footer

--- projetoestagio/src/components/InfoFooter/index.jsx
+++ projetoestagio/src/components/InfoFooter/index.jsx
@@ -7,7 +7,7 @@ const InfoFooter =()=>{
             <p>Faculdade de Tecnologia da Zona Leste</p>
             <p>Avenida Águia de Haia, 2983 CEP 03694-000</p>
             <p>Telefones: (11) 2049-2600 / (11) 2026-1303</p>
-            <p>Desenvolvido por <a className={styles.link} href="https://www.linkedin.com/in/antonio-pinheiro-9a9a35213">Antonio Gabriel</a>, <a className={styles.link} href="https://www.linkedin.com/in/felipecolettii">Felipe Coletti</a> e <a className={styles.link} href="https://www.linkedin.com/in/karoline-souza-225058213">Karoline de Souza</a>. &copy; 2023</p>
+            <p>Desenvolvido por <a className={styles.link} href="https://www.linkedin.com/in/antonio-pinheiro-9a9a35213">Antonio Gabriel</a> e <a className={styles.link} href="https://www.linkedin.com/in/felipecolettii">Felipe Coletti</a>. &copy; 2023</p>
         </div>
     )
 }
