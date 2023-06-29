fe3fbb5f6b767b26bd754c5cb6cd009a821072f8
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-06-29 15:11:09 -0300

    Ajuste do header

--- projetoestagio/src/components/Header/index.jsx
+++ projetoestagio/src/components/Header/index.jsx
@@ -2,21 +2,21 @@ import React,{ useState } from "react";
 import styles from "./styles.module.css";
 import LogoFatec from "../../assets/images/LogoFatecZL.png";
 import { Icon } from "@iconify/react";
-import NavBarMobile from "../NavBarMobile";
+/*import NavBarMobile from "../NavBarMobile";*/
 import BarraPesquisa from "../BarraPesquisa";
 import BotaoNotificacoes from "../BotaoNotificacoes";
 
 const Header =()=>{
-    const [navBar, setNavBar] = useState(false)
+    /*const [navBar, setNavBar] = useState(false)
 
-    const showNavBar = () => setNavBar(!navBar)
+    const showNavBar = () => setNavBar(!navBar)*/
 
     return(
         <div className={styles.caixa}>
-            <button className={styles.menuHamburguer} onClick={showNavBar}>
+            {/*<button className={styles.menuHamburguer} onClick={showNavBar}>
                 <Icon icon="ic:baseline-menu"/>
             </button>
-            {navBar && <NavBarMobile active={setNavBar}/>}
+            {navBar && <NavBarMobile active={setNavBar}/>}*/}
             <BarraPesquisa/>
             <ul className={styles.caixaFerramentas}>
                 <li className={styles.item}><BotaoNotificacoes/></li>
