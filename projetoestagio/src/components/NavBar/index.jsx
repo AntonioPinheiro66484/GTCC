875bc8ddb00dae014d3dd30bdd97e2a57c1235fa
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-06-29 16:42:08 -0300

    Ajuste nas opções da barra de navegação

--- projetoestagio/src/components/NavBar/index.jsx
+++ projetoestagio/src/components/NavBar/index.jsx
@@ -17,9 +17,9 @@ const NavBar =(/*{active}*/)=>{
                     </button>
                 </div>*/}
                 <ul className={styles.paiItens}>
-                    <li className={styles.item}><a className={styles.linkItem} href="/trabalho"><Icon className={styles.iconeItem} icon="octicon:project-roadmap-24"/>Trabalho</a></li>
-                    <li className={styles.item}><a className={styles.linkItem} href="/orientadores"><Icon className={styles.iconeItem} icon="mdi:human-male-board"/>Orientadores</a></li>
+                    <li className={styles.item}><a className={styles.linkItem} href="/trabalho"><Icon className={styles.iconeItem} icon="octicon:project-roadmap-24"/>Meu trabalho</a></li>
                     <li className={styles.item}><a className={styles.linkItem} href="/trabalhos"><Icon className={styles.iconeItem} icon="mdi:note-multiple-outline"/>Trabalhos</a></li>
+                    <li className={styles.item}><a className={styles.linkItem} href="/orientadores"><Icon className={styles.iconeItem} icon="mdi:human-male-board"/>Orientadores</a></li>
                     {/*<li className={styles.item}><a className={styles.linkItem} href="/solicitacoes"><Icon className={styles.iconeItem} icon="material-symbols:notifications-outline"/>Notificações</a></li>*/}
                     <li className={styles.item}><a className={styles.linkItem} href="/tipos-de-trabalho"><Icon className={styles.iconeItem} icon="ph:squares-four-light"/>Tipos de trabalho</a></li>
                     <li className={styles.item}><a className={styles.linkItem} href="http://suporte.fateczl.edu.br" target="_blank" rel="noopener noreferrer"><Icon className={styles.iconeItem} icon="formkit:help"/>Ajuda</a></li>
